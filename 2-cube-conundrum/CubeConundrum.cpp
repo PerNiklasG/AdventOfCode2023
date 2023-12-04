9a010cb61df785678df4d01cec5c5346009c2ed9
PerNiklasG <147505582+PerNiklasG@users.noreply.github.com>
2023-12-04 08:47:38 +0100

    Added day 2, could not do it on my own

--- 2-cube-conundrum/CubeConundrum.cpp
+++ 2-cube-conundrum/CubeConundrum.cpp
@@ -1,122 +1,91 @@
+/*
+    Solution copied from someone else.
+*/
+
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <numeric>
+#include <cctype>
+#include <map>
+#include <regex>
 
-bool checkIfTooMany(std::string s, int n);
+struct game_t{
+    int id;
+    std::vector<int> red;
+    std::vector<int> green;
+    std::vector<int> blue;
+};
+
+using games_t = std::vector<game_t>;
+
+games_t load_input(const std::string& file){
+    games_t ret;
+    std::ifstream fs(file);
+    std::string line;
+    while (std::getline(fs, line)) {
+        game_t game;
+        sscanf_s(line.c_str(), "Game %d\n", &game.id); 
+        std::string str = line.substr(line.find(":")+1);
+        std::regex word_regex("[0-9]+.\\w");
+        for(auto it=std::sregex_iterator(str.begin(), str.end(), word_regex); it!=std::sregex_iterator(); it++){
+            int num_blocks; char block_color;
+            sscanf_s(it->str().c_str(), "%d %c\n", &num_blocks, &block_color, 1);  
+            if(block_color == 'r'){
+                game.red.push_back(num_blocks);
+            }else if(block_color == 'g'){
+                game.green.push_back(num_blocks);
+            }else if(block_color == 'b'){
+                game.blue.push_back(num_blocks);
+            }
+        }
+        ret.push_back(game);
+    }
+    return ret;
+}
+
+int part1(const games_t& games)
+{
+    auto test_color = [](auto& colors, int max_color){
+        return std::all_of(colors.begin(), colors.end(), [&](auto& num){ return num <= max_color; });
+    };
+
+    int sum = 0;
+    for(auto& game : games){
+        if(test_color(game.red, 12) && test_color(game.green, 13) && test_color(game.blue, 14)){
+            sum += game.id;
+        }
+    }
+
+    return sum;
+}
+
+int part2(const games_t& games)
+{
+    auto max_color = [](auto& colors){
+        return *std::max_element(colors.begin(), colors.end());
+    };
+
+    int sum = 0;
+    for(auto& game : games){
+        sum += max_color(game.red) * max_color(game.green) * max_color(game.blue);
+    }
+
+    return sum;
+}
 
 int main()
 {
-    std::ifstream infile("sample.txt");
-    std::string line;
-    int res;
+    auto test_values = load_input("sample.txt");
+    auto actual_values = load_input("cubeconundrum_input.txt");
 
-    if (infile.is_open())
-    {
-        while (getline(infile, line))   // read each line from file
-        {                                                
-            std::string::size_type pos = line.find(':'); // Find first ':', i.e. position of where the first "pair" starts
-            std::string game = line.substr(0, pos + 1);
-            std::cout << "line: " << line << std::endl;
+    std::cout << "part1: " << part1(test_values) << std::endl;
+    std::cout << "part1: " << part1(actual_values) << std::endl;
 
-            std::string game_num;
-            for(int i = 0; i < game.length(); i++)
-            {
-                if(isdigit(game[i]))
-                {
-                    game_num += game[i];
-                }
-            }
-            
-            int num = std::stoi(game_num);
-            std::cout << "Game num: " << num << std::endl;
+    std::cout << "part2: " << part2(test_values) << std::endl;
+    std::cout << "part2: " << part2(actual_values) << std::endl;
 
-            
-            
-            std::string set = line.substr(pos + 1);
-    
-            std::cout << "Set is: " << set << std::endl;
-            
-            for(int i = 0; i < set.length(); i++)
-            {
-                static std::string number;
-                static std::string color;
-     
-                while (set[i] != ',' || i < set.length())
-                {
-                    if(isdigit(set[i]))
-                    {
-                        num += set[i];
-                    }
-                    else if(isalpha(set[i]))
-                    {
-                        color += set[i];
-                    }
-                    i++;
-                    //std::cout << "num: " << number << " color: " << color << std::endl;
-                }
-                std::cout << "num: " << number << " color: " << color << std::endl;
-            }
-
-            // for(int i = 0; i < pair.length(); i++)
-            // {
-            //     if(isdigit(line[i]))
-            //     {
-            //         std::string number = "";
-            //         number = line[i];
-            //         current_pair = stoi(number);
-            //     }
-            // }
-
-            // std::cout << "pair: " << current_pair << " pair: " << pair << std::endl;
-            // for (int i = 0; i < pair.length(); i++) // iterate through each char
-            // {
-            //     if (ispunct(pair[i])) // new pair
-            //     {
-            //         std::cout << "pair: " << pair << " num: " << num << std::endl;
-            //         is_valid = checkIfTooMany(pair, num);
-            //         pair = "";
-            //     }
-            //     else if (isdigit(pair[i])) // Find integer
-            //     { 
-            //         std::cout << "Num found: " << num << std::endl;
-            //         num = static_cast<int>(pair[i]);
-            //     }
-            //     else if(isalpha(pair[i]))
-            //     {
-            //         pair += pair[i];
-            //     }
-            // }
-
-            // if(is_valid)
-            // {
-            //     res += current_pair;
-            // }
-        }
-    }
-}
-
-bool checkIfTooMany(std::string s, int n)
-{
-    if (s == "red")
-    {
-        if (n <= 12)
-        {
-            return true;
-        }
-    }
-    else if (s == "green")
-    {
-        if (n <= 13)
-        {
-            return true;
-        }
-    }
-    else if (s == "blue")
-    {
-        if (n <= 14)
-        {
-            return true;
-        }
-    }
-    return false;
+    return 0;
 }
